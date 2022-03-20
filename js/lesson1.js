5e2ee61e78df604a16803eb0a06facab1cb87300
vdubovsky <vladimir.dubovsky@outlook.com>
2022-03-20 14:05:57 +0300

    continue learning js

---
+++ js/lesson1.js
@@ -0,0 +1,20 @@
+var name = 'Vova'
+
+const lastname = 'Dub'
+const age = 26 
+
+// Don't use var
+// Semicolon up to you
+// Use camelCase
+// const can't be changed?
+
+let _ = 'asd'
+let $ = 77
+let withNum5 = '5'
+
+//let 5num = '5' - error
+
+console.log('My name is ' + name + ' ' + lastname + '. I am ' + age + ' old.')
+console.log(age)
+console.log(age.toString())
+
