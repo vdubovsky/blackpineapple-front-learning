ac97dfa38db8df0050998ddbae6b08aac18e8823
vdubovsky <vladimir.dubovsky@outlook.com>
2022-03-27 18:25:15 +0300

    continue javascript learning

--- js/lesson1.js
+++ js/lesson1.js
@@ -137,31 +137,74 @@
 
 // console.log(person)
 
-const vova = {
-	name: 'Vova', 
-	age: 32, 
-	languages: ['Ru', 'By', 'Eng', 'Esp'], 
-	hasWife: true, 
-	hello: function(){
-		console.log('Hello')
-	}
-}
+// const vova = {
+//	name: 'Vova', 
+//	age: 32, 
+//	languages: ['Ru', 'By', 'Eng', 'Esp'], 
+//	hasWife: true, 
+//	hello: function(){
+//		console.log('Hello')
+//	}
+//}
 
-console.log(vova)
-console.log(vova.name)
-console.log(vova['age'])
-const langs = 'languages'
-console.log(vova[langs])
+// console.log(vova)
+// console.log(vova.name)
+// console.log(vova['age'])
+// const langs = 'languages'
+// console.log(vova[langs])
 
-vova.hello()
+// vova.hello()
 
-vova.isProgrammer = true
+// vova.isProgrammer = true
 
-console.log(vova)
+// console.log(vova)
 
+// 10. Numbers
 
+// const num = 42 // integer
+// const float = 42.42 // float
+// const pow = 10e3
+//
+// console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
+// console.log('Math.pow 53', Math.pow(2, 53) - 1)
+// console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
+// console.log('MAX_VALUE', Number.MAX_VALUE)
+// console.log('MIN_VALUE', Number.MIN_VALUE)
+// console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
+// console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
+// console.log('2 / 0', 2 / 0)
+// console.log(Number.NaN) // Not A Number
+// console.log(typeof NaN)
+// const weird = 2 / undefined
+// console.log(Number.isNaN(weird))
+// console.log(Number.isNaN(42))
+// console.log(Number.isFinite(Infinity))
+// console.log(Number.isFinite(42))
+//
+// const stringInt = '40'
+// const stringFloat = '40.42'
+// console.log(Number.parseInt(stringInt) + 2)
+// console.log(parseInt(stringInt) + 2)
+// console.log(Number(stringInt) + 2)
+// console.log(+stringInt + 2)
+// console.log(parseFloat(stringFloat) + 2)
+// console.log(+stringFloat + 2)
 
+// console.log(0.4 + 0.2) // 0.6
+// console.log(+(0.4 + 0.2).toFixed(1))
+// console.log(parseFloat((0.4 + 0.2).toFixed(1)))
 
+// 11. BigInt
 
+// console.log(9007199254740991)
+// console.log(9007199254740991999)
+// console.log(typeof 9007199254740991999)
+// console.log(typeof 9007199254740991999n)
+
+// console.log(9007199254740991999n - 12312312312n)
+// console.log(10n-4) Error
+
+console.log(parseInt(5n) + 1)
+console.log(5n + BigInt(1))
 
 
